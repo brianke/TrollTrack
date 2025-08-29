ecb693264473b7201b30db6b1fc368ca18bdc963
brianke <brianke@gmail.com>
2025-08-28 21:23:01 -0400

    Fix issue with building APK

--- TrollTrack/MauiProgram.cs
+++ TrollTrack/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System.Reflection;
 using TrollTrack.Configuration;
@@ -12,6 +13,7 @@ namespace TrollTrack
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
