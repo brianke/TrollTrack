a0fd41e77afd0f4234a37b29154f6b95cb3b30ca
brianke <brianke@gmail.com>
2025-09-22 18:39:26 -0400

    Added RouteConstants.cs

---
+++ TrollTrack/Configuration/RouteConstants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrollTrack.Configuration
+{
+    public static class RouteConstants
+    {
+        public const string Catch = "//catch";
+        public const string Trolling = "//trolling";
+        public const string History = "//history";
+        public const string Lures = "//lures";
+        public const string Analytics = "//analytics";
+    }
+}
